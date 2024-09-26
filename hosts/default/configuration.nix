6339ae4d2217c9319166cd9114a1e4513b73bb0e
Aras14HD <aras14k5@gmail.com>
2024-09-26 07:52:12 +0200

    install mpv, audacity, vlc, pciutils, usbutils

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -182,6 +182,8 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     helix
+    pciutils
+    usbutils
     git
     nil
     keepassxc
