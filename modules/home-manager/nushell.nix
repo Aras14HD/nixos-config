fc01dccb89b7d1e0e2f91041938d1987bc35a77d
Aras14HD <aras14k5@gmail.com>
2025-03-22 12:45:02 +0100

    move config in nix

--- modules/home-manager/nushell.nix
+++ modules/home-manager/nushell.nix
@@ -13,6 +13,20 @@
     extraConfig = ''
       $env.PATH = ($env.PATH | append /home/jorim/Applications/scripts)
       $env.EDITOR = hx
+      $env.config = {
+        hooks: {
+          pre_prompt: [{ ||
+            if (which direnv | is-empty) {
+              return
+            }
+
+            direnv export json | from json | default {} | load-env
+            if 'ENV_CONVERSIONS' in $env and 'PATH' in $env.ENV_CONVERSIONS {
+              $env.PATH = do $env.ENV_CONVERSIONS.PATH.from_string $env.PATH
+            }
+          }]
+        }
+      }
     '';
   };
   programs.carapace.enable = true;
