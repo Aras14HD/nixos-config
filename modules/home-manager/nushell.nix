cfb7a355758eb930535c8520b1721122a756f669
Aras14HD <aras14k5@gmail.com>
2025-03-22 12:28:49 +0100

    fix nushell env

--- modules/home-manager/nushell.nix
+++ modules/home-manager/nushell.nix
@@ -10,7 +10,10 @@
       edit-config="cd /home/jorim/.config/nixos && $EDITOR && git add . && git commit && git push && nh os switch; nh clean all -K 14d -k 5; cd -";
       upgrade="nix flake update --flake /home/jorim/.config/nixos && git -C /home/jorim/.config/nixos add flake.lock && git -C /home/jorim/.config/nixos commit -m \"update flake\" && git -C /home/jorim/.config/nixos push && nh os switch; nh clean all -K 14d -k 5; flatpak update;";
     };
-    envExtra = "export EDITOR=hx\nexport PATH=$PATH:/home/jorim/Applications/scripts";
+    extraConfig = ''
+      $env.PATH = ($env.PATH | append /home/jorim/Applications/scripts)
+      $env.EDITOR = hx
+    '';
   };
   programs.carapace.enable = true;
   programs.carapace.enableNushellIntegration = true;
