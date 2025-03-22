0651c5dc37ed902aefb0b9cf0e74cf1556462051
Aras14HD <aras14k5@gmail.com>
2025-03-22 13:42:57 +0100

    move complex aliases to functions

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -19,6 +19,7 @@
   hardware.bluetooth.enable = true; # enables support for Bluetooth
   hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
   hardware.sensor.iio.enable = true; # automatic backlight
+
   
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
