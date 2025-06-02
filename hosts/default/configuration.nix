f2ca65cd2df45a30957a7c94ca4ba2542dcd0996
Aras14HD <aras14k5@gmail.com>
2025-06-02 19:39:59 +0200

    remove thefuck

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -208,7 +208,6 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     fastfetch
-    thefuck
     helix
     pciutils
     usbutils
