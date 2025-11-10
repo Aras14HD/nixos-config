25c8cd77a047d45a92f0c1b38a5844f8d0c1265a
Aras14HD <aras14k5@gmail.com>
2025-11-10 16:23:19 +0100

    update git email

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -3,12 +3,11 @@
   programs.git = {
     settings = {
       user.name = "Aras14HD";
-      user.email = "aras14k5@gmail.com";
+      user.email = "aras@aras14.dev";
       push.autoSetupRemote = true;
       commit.gpgsign = true;
       gpg.format = "ssh";
       user.signingkey = "/home/jorim/.ssh/id_ecdsa_sk";
-      # user.signingkey = "919386A939093546";
     };
   };
 }
