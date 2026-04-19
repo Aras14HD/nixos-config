65ed1960b3c65db3a3c8a47a5addd1fe889998fb
jorim <jorim.kowalewski@stud.h-da.de>
2026-04-19 16:44:07 +0200

    zsh nixpks allow unfree

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -10,7 +10,7 @@
       edit-config="cd /home/jorim/.config/nixos && $EDITOR && git add . && git commit && git push && nh os switch; nh clean all -K 14d -k 5; cd -";
       upgrade="nix flake update --flake /home/jorim/.config/nixos && git -C /home/jorim/.config/nixos add flake.lock && git -C /home/jorim/.config/nixos commit -m \"update flake\" && git -C /home/jorim/.config/nixos push && nh os switch; nh clean all -K 14d -k 5; flatpak update;";
     };
-    envExtra = "export EDITOR=hx\nexport PATH=$PATH:/home/jorim/Applications/scripts";
+    envExtra = "export EDITOR=hx\nexport PATH=$PATH:/home/jorim/Applications/scripts\nexport NIXPKGS_ALLOW_UNFREE=1";
     oh-my-zsh = {
       enable = true;
       plugins = ["git" "colored-man-pages" "colorize" "common-aliases" "cp" "direnv" "eza" "man" "rust"];
