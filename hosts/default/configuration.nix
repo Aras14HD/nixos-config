e20e6163d7fbc5f667a7ac3db68a58adfca5b45f
Aras14HD <aras@aras14.dev>
2026-07-04 11:59:07 +0200

    switch to latest kernel

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -34,6 +34,8 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
   networking.hostName = "jorim-laptop"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
